325e80c86fdaa319ce80ccf09eb58a11a4f0146e
tails5555 <tails5555@naver.com>
2018-05-06 19:10:09 +0900

    Add Select Indexes Buttons 사용자가 권장하는 선택 삭제를 이용해서 Index 목록들을 통해 실제 서버에서 받아서 삭제를 하도록 추가하였음.

--- src/component/photo/PhotoList.js
+++ src/component/photo/PhotoList.js
@@ -5,18 +5,44 @@ import './photoBlock.css';
 // PhotoList는 각 앨범 별 사진 정보들을 가져와서 보여주는 Component이다.
 class PhotoList extends Component{
     // 메뉴에서 앨범을 선택하고 난 뒤에 사진 목록을 추출한다.
+    constructor(props){
+        super(props);
+        this.state = { selectIndex : [] }
+    }
+
+    handleClick(photoId){
+        let tmpIndexes = this.state.selectIndex.slice();
+        if(!tmpIndexes.includes(photoId)){
+            tmpIndexes.push(photoId);
+        }else{
+            for(var i in tmpIndexes){
+                if(tmpIndexes[i] === photoId){
+                    tmpIndexes.splice(i,1);
+                    break;
+                }
+            }
+        }
+        tmpIndexes.sort(function(a, b){
+            return a - b;
+        });
+        this.setState({ selectIndex : tmpIndexes });
+    }
+
+
     componentWillMount(){
         this.props.fetchPhotos(this.props.albumId);
     }
     // 다른 메뉴로 옮겨지는 경우에는 일시적으로 사진 목록에 대해 초기화를 한다.
     componentWillUnmount(){
         this.props.resetFetchPhotos();
+        this.setState({ selectIndex : [] });
     }
     // 사용자가 앨범 목록들 중에서 다른 앨범을 선택할 때는 이에 맞춰서 변경이 되도록 반영한다.
     componentWillReceiveProps(nextProps) {
         if(this.props.albumId !== nextProps.albumId){
             this.props.resetFetchPhotos();
             this.props.fetchPhotos(nextProps.albumId);
+            this.setState({ selectIndex : [] });
         }
     }
     // shouldComponentUpdate에 대해서는 외부에서 적용을 시켜서 여기서는 할 필요가 없다.
@@ -36,6 +62,12 @@ class PhotoList extends Component{
                 return(
                     <div className="col-md-2 photoElement" key={idx}>
                         <PhotoElement photoData={photo} />
+                        {
+                            this.state.selectIndex.includes(photo.id) ?
+                                <button type="button" className="btn btn-block btn-warning" onClick={() => this.handleClick(photo.id)}>취소하기</button> :
+                                <button type="button" className="btn btn-block btn-primary" onClick={() => this.handleClick(photo.id)}>선택하기</button>
+                        }
+
                     </div>
                 );
             });
@@ -45,9 +77,14 @@ class PhotoList extends Component{
             photoElements = <h2 className="hn">유효하지 않는 앨범을 요청했습니다...</h2>;
         }
         return(
-            <div className="row photoBlock">
-                {photoElements}
-            </div>
+            <form>
+                <div className="row photoBlock">
+                    <div className="col-md-12">
+                        <button type="button" className="btn btn-block btn-danger">파일 선택 삭제</button>
+                    </div>
+                    {photoElements}
+                </div>
+            </form>
         )
     }
 }
